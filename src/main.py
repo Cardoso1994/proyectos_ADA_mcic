56c693d640da023174264795c1d72caf34e6e6f7
Marco Cardoso <marcoacardosom@gmail.com>
2021-04-26 10:21:32 -0500

    update main

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@
 #
 # Desarrollador: Marco Antonio Cardoso Moreno
 #
-# Main. Generador de Mallas. Proyecto 01 ADA
+# Main. Generador de Grafos y árboles DFS y BFS. Proyecto 02 ADA
 #
 # Abril 2021
 #
@@ -23,33 +23,69 @@ from generador_grafos import grafoMalla, \
 
 
 def main():
-    path = "/home/cardoso/garbage/"
+    path = "/home/cardoso/garbage/grafos/500/"
 
     nodos = 500
     nodos_malla = (25, 20)
 
-    m_erdos = 2550
-    p_gilbert = 0.1
-    r_geografico = 0.1
+    m_erdos = 2000
+    p_gilbert = 0.2
+    r_geografico = 0.2
     d_barabasi = 7
 
     g = grafoMalla(*nodos_malla)
     g.to_graphviz(path + g.id + ".gv")
+    bfs = g.BFS(Nodo(0))
+    bfs.to_graphviz(path + bfs.id + ".gv")
+    dfs = g.DFS_R(Nodo(0))
+    dfs.to_graphviz(path + dfs.id + ".gv")
+    dfs_i = g.DFS_I(Nodo(0))
+    dfs_i.to_graphviz(path + dfs_i.id + ".gv")
 
     g = grafoErdosRenyi(nodos, m_erdos)
     g.to_graphviz(path + g.id + ".gv")
+    bfs = g.BFS(Nodo(0))
+    bfs.to_graphviz(path + bfs.id + ".gv")
+    dfs = g.DFS_R(Nodo(0))
+    dfs.to_graphviz(path + dfs.id + ".gv")
+    dfs_i = g.DFS_I(Nodo(0))
+    dfs_i.to_graphviz(path + dfs_i.id + ".gv")
 
     g = grafoGilbert(nodos, p_gilbert, dirigido=False, auto=False)
     g.to_graphviz(path + g.id + ".gv")
+    bfs = g.BFS(Nodo(0))
+    bfs.to_graphviz(path + bfs.id + ".gv")
+    dfs = g.DFS_R(Nodo(0))
+    dfs.to_graphviz(path + dfs.id + ".gv")
+    dfs_i = g.DFS_I(Nodo(0))
+    dfs_i.to_graphviz(path + dfs_i.id + ".gv")
 
     g = grafoGeografico(nodos, r_geografico)
     g.to_graphviz(path + g.id + ".gv")
+    bfs = g.BFS(Nodo(0))
+    bfs.to_graphviz(path + bfs.id + ".gv")
+    dfs = g.DFS_R(Nodo(0))
+    dfs.to_graphviz(path + dfs.id + ".gv")
+    dfs_i = g.DFS_I(Nodo(0))
+    dfs_i.to_graphviz(path + dfs_i.id + ".gv")
 
     g = grafoBarabasiAlbert(nodos, d_barabasi, auto=False)
     g.to_graphviz(path + g.id + ".gv")
+    bfs = g.BFS(Nodo(0))
+    bfs.to_graphviz(path + bfs.id + ".gv")
+    dfs = g.DFS_R(Nodo(0))
+    dfs.to_graphviz(path + dfs.id + ".gv")
+    dfs_i = g.DFS_I(Nodo(0))
+    dfs_i.to_graphviz(path + dfs_i.id + ".gv")
 
     g = grafoDorogovtsevMendes(nodos, dirigido=False)
     g.to_graphviz(path + g.id + ".gv")
+    bfs = g.BFS(Nodo(0))
+    bfs.to_graphviz(path + bfs.id + ".gv")
+    dfs = g.DFS_R(Nodo(0))
+    dfs.to_graphviz(path + dfs.id + ".gv")
+    dfs_i = g.DFS_I(Nodo(0))
+    dfs_i.to_graphviz(path + dfs_i.id + ".gv")
 
 if __name__ == "__main__":
     main()
